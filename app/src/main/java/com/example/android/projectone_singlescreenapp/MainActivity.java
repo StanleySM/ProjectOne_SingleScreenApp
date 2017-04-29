129971b005de9c4aa6fe9ed54bc29d670ba72a0f
StanleySM <standa.malec@gmail.com>
2017-04-29 13:33:12 +0200

    Up

---
+++ app/src/main/java/com/example/android/projectone_singlescreenapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.projectone_singlescreenapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void startReservation(View view) {
+        // Do something in response to button click ONLINE RESERVATION ... in progress..
+
+    }
+
+}
